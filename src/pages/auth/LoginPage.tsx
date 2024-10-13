f858a77f08cc8427a3e2c4a1d392316693a9d121
Lycrat <Panya_R@outlook.com>
2024-10-13 17:41:17 +0100

    add registration

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,9 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../components";
 import { Stack, TextField, Box, Grid } from "@mui/material";
 import { Header } from "../../components/index";
 import { Link } from "react-router-dom";
 const LoginPage = () => {
+  const [showLogin, setShowLogin] = useState(true);
+  let loginContent;
+  let registerContent;
+  const formClass =
+    "font-bold shadow appearance-none border rounded-3xl w-64 h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline";
+  if (showLogin) {
+    loginContent = (
+      <form className="h-50">
+        <Stack spacing={2} justifyContent="center" alignItems="center">
+          <input
+            className="font-bold shadow appearance-none border rounded-3xl w-64 h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
+            id="email"
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            className="font-bold shadow appearance-none border rounded-3xl w-64 h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
+            id="password"
+            type="text"
+            placeholder="Password"
+          />
+          <Link to="#" className="text-center hover:underline">
+            Read <span className="font-bold">Terms and Conditions</span>
+          </Link>
+          <Stack direction="row" className="w-full" justifyContent="center">
+            <Button
+              label="Sign in"
+              buttonType="primary"
+              onClick={() => {
+                alert("clicked Sign in");
+              }}
+              h="h-14"
+              w="w-24"
+            ></Button>
+            <Button
+              label="Register"
+              buttonType="secondary"
+              onClick={() => {
+                setShowLogin(false);
+              }}
+              h="h-14"
+              w="w-24"
+            ></Button>
+          </Stack>
+        </Stack>
+      </form>
+    );
+  } else {
+    registerContent = (
+      <form className="h-50">
+        <Stack spacing={2} justifyContent="center" alignItems="center">
+          <Stack spacing={2} justifyContent="center" direction="row">
+            <input
+              className="font-bold shadow appearance-none border rounded-3xl w-64 h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
+              id="firstName"
+              type="text"
+              placeholder="First Name"
+            ></input>
+            <input
+              className="font-bold shadow appearance-none border rounded-3xl w-64 h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
+              id="lastName"
+              type="text"
+              placeholder="Last Name"
+            ></input>
+          </Stack>
+          <input
+            className="font-bold shadow appearance-none border rounded-3xl w-full h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
+            id="email"
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            className="font-bold shadow appearance-none border rounded-3xl w-full h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
+            id="password"
+            type="text"
+            placeholder="Password"
+          />
+          <Link to="#" className="text-center hover:underline">
+            Read <span className="font-bold">Terms and Conditions</span>
+          </Link>
+          <Stack direction="row" className="w-full" justifyContent="center">
+            <Button
+              label="Register"
+              buttonType="primary"
+              onClick={() => {
+                alert("clicked Sign in");
+              }}
+              h="h-14"
+              w="w-50"
+            ></Button>
+            <Button
+              label="Sign in"
+              buttonType="secondary"
+              onClick={() => {
+                setShowLogin(!showLogin);
+              }}
+              h="h-14"
+              w="w-24"
+            ></Button>
+          </Stack>
+        </Stack>
+      </form>
+    );
+  }
   return (
     <div>
       <Header></Header>
@@ -15,58 +119,21 @@ const LoginPage = () => {
               alignItems="center"
               className="flex h-full rounded-l-full bg-sky "
             >
-              <form className="h-50">
-                <Stack spacing={2} justifyContent="center" alignItems="center">
-                  <input
-                    className="font-bold shadow appearance-none border rounded-3xl w-64 h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
-                    id="email"
-                    type="text"
-                    placeholder="Email"
-                  />
-                  <input
-                    className="font-bold shadow appearance-none border rounded-3xl w-64 h-16 py-2 px-3 text-grey-700 leading-right focus:outline-none focus:shadow-outline"
-                    id="password"
-                    type="text"
-                    placeholder="Password"
-                  />
-                  <Link to="#" className="text-center hover:underline">
-                    Read <span className="font-bold">Terms and Conditions</span>
-                  </Link>
-                  <Stack
-                    direction="row"
-                    className="w-full"
-                    justifyContent="center"
-                  >
-                    <Button
-                      label="Sign in"
-                      buttonType="primary"
-                      onClick={() => {
-                        alert("clicked Sign in");
-                      }}
-                      h="h-14"
-                      w="w-24"
-                    ></Button>
-                    <Button
-                      label="Register"
-                      buttonType="secondary"
-                      onClick={() => {
-                        alert("clicked Sign in");
-                      }}
-                      h="h-14"
-                      w="w-24"
-                    ></Button>
-                  </Stack>
-                </Stack>
-              </form>
+              {loginContent}
             </Box>
           </Grid>
           <Grid item xs={6}>
-            <Box className="outline outline-1 outline-black h-full"></Box>
+            <Box
+              justifyContent="center"
+              alignItems="center"
+              className="flex h-full outline outline-1 outline-black h-full"
+            >
+              {registerContent}
+            </Box>
           </Grid>
         </Grid>
       </Box>
     </div>
   );
 };
-
 export default LoginPage;
